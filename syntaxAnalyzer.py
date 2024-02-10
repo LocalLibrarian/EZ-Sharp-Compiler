b95c9e3b4fb8f4ea7166fdd9bc22774a8569e6e7
Travis Shirley <44935136+LocalLibrarian@users.noreply.github.com>
2024-02-10 15:05:47 -0500

    Created initial compiler Phase 2 files
    - Created syntax analysis main file
    - Created LL(1) Parser Table

---
+++ syntaxAnalyzer.py
@@ -0,0 +1,6 @@
+#Constants
+TABLEFILE = 'table.csv' #CSV file for LL(1) table
+ERRORFILE = 'errorLogSyntax.txt'
+OUTFILE = 'outputSyntax.txt'
+INFILE = 'outputLexical.txt' #From lexicalAnalyzer.py
+EPSILON = 'EPSILON'
